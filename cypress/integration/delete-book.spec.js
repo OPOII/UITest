0b85e00e57ed16447649f21699ef2a5d5258653b
OPOII <41926039+OPOII@users.noreply.github.com>
2021-03-27 22:25:04 -0500

    Update delete-book.spec.js

--- cypress/integration/delete-book.spec.js
+++ cypress/integration/delete-book.spec.js
@@ -1,24 +1,18 @@
-const bookAuthor="Geraciona.";
-const bookTitle="Hunter";
-describe('when the user wants to update a book',()=>{
+const bookAuthor="Slack winter";
+const bookTitle="Marcianito";
+describe('When the user wants to delete a book', ()=>{
     before(()=>{
-        cy.visit('https://front-books-icesi.herokuapp.com');
-        cy.get('#addBook').click();
-        cy.get('#name').type(bookTitle);
-        cy.get('#author').type(bookAuthor);
-        cy.get('#saveButton').click();
+        cy.visit("https://front-books-icesi.herokuapp.com");
         cy.contains('10 / page').click();
         cy.contains('50 / page').click();
-        cy.get("table").contains('tr', bookTitle).invoke("index").then((i) =>{
-          
+    })
+    it('The book should not exist in the library',()=>{
+        cy.get("table").contains('tr', bookAuthor).invoke("index").then((i) =>{
             cy.get(`:nth-child(${i+1}) > .ant-table-selection-column > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input`).click();
-        });
-        cy.get('#delteBook').click();
+        })
+        cy.get('[nztype="default"]').click();
+        cy.get("table").contains('tr', bookTitle).should('not.exist');
+   
+    });
 
-    });
-    it("The book must be modified in the list of books",()=>{
-        cy.get('table').contains('td',bookAuthor).should('not.exist');
-        cy.get('table').contains('td',bookTitle).should('not.exist');
-        
-    });
-});
+})
